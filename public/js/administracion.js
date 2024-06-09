7e4f0dd210d79d0b3aa2e3bde48687fe6fe0ec63
Will Mattos <willmattos.c@gmail.com>
2024-06-09 19:13:01 +0200

    administracion

---
+++ public/js/administracion.js
@@ -0,0 +1,106 @@
+$("input[type='file']").on("change", function () {
+  $(this)
+    .parent()
+    .next()
+    .attr("src", URL.createObjectURL($(this)[0].files[0]));
+});
+
+$(".productos-agregar").on("click", function () {
+  toggle(".container_productos_agregar");
+  $(".container_productos_eliminar").css("display", "none");
+  $(".container_usuarios").css("display", "none");
+});
+$(".productos-eliminar").on("click", function () {
+  toggle(".container_productos_eliminar");
+  $(".container_productos_agregar").css("display", "none");
+  $(".container_usuarios").css("display", "none");
+});
+$(".usuario-eliminar").on("click", function () {
+  toggle(".container_usuarios");
+  $(".container_productos_agregar").css("display", "none");
+  $(".container_productos_eliminar").css("display", "none");
+});
+function toggle(param) {
+  $(param).slideToggle();
+}
+function buscarUsuario(nombre) {
+  $.ajax({
+    type: "post",
+    url: ruta_buscador_usuario,
+    data: {
+      usuario: nombre,
+    },
+    dataType: "json",
+    success: function (response) {
+      $(".container_users").empty();
+      response.forEach((element) => {
+        $(".container_users").append(`<div>
+				<img src="${element.photo}" alt="foto">
+				<div>${element.email}</div>
+				<button class="eliminar-user" data-id="${element.id}">Eliminar</button>
+			</div>`);
+      });
+      console.log(response);
+    },
+  });
+}
+function buscarProducto(nombre) {
+  $.ajax({
+    type: "post",
+    url: ruta_buscador_producto,
+    data: {
+      producto: nombre,
+    },
+    dataType: "json",
+    success: function (response) {
+      $(".container_productos").empty();
+      response.forEach((element) => {
+        $(".container_productos").append(`<div>
+				<img src="${element.imagen}" alt="foto">
+				<div>${element.nombre}</div>
+				<button class="eliminar-product" data-id="${element.id}">Eliminar</button>
+			</div>`);
+      });
+    },
+  });
+}
+$(document).on("click", ".eliminar-product", function () {
+  let id = $(this).data("id");
+  $(this).parent().remove();
+  $.ajax({
+    type: "post",
+    url: ruta_eliminar_productos,
+    data: {
+      id: id,
+    },
+    dataType: "json",
+    success: function (response) {
+      console.log(response);
+    },
+  });
+});
+$(document).on("click", ".eliminar-user", function () {
+  let id = $(this).data("id");
+  $(this).parent().remove();
+  $.ajax({
+    type: "post",
+    url: ruta_eliminar_usuario,
+    data: {
+      id: id,
+    },
+    dataType: "json",
+    success: function (response) {
+      console.log(response);
+    },
+  });
+});
+
+$("#buscador-producto").keyup(function (e) {
+  var nombre = $(this).val();
+  buscarProducto(nombre);
+});
+
+$("#buscador-usuario").keyup(function (e) {
+  var nombre = $(this).val();
+  buscarUsuario(nombre);
+});
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,20 +4,26 @@ namespace App\Controller;
 
 use App\Entity\Producto;
 use App\Repository\ProductoRepository;
+use App\Repository\UsuarioRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 class HomeController extends AbstractController
 {
     private EntityManagerInterface $entityManager;
+    private ProductoRepository $repo_producto;
+    private UsuarioRepository $repo_usuario;
 
-    public function __construct(EntityManagerInterface $entityManager)
+    public function __construct(EntityManagerInterface $entityManager, ProductoRepository $repo_producto, UsuarioRepository $repo_usuario)
     {
         $this->entityManager = $entityManager;
+        $this->repo_producto = $repo_producto;
+        $this->repo_usuario = $repo_usuario;
     }
     #[Route('/', name: 'home')]
     public function index(): Response
@@ -183,5 +189,123 @@ class HomeController extends AbstractController
             'filterName' => ucfirst($marca), // Capitalizamos la primera letra para la presentación
         ]);
     }
+    #[Route('/administracion', name: 'administracion')]
+    public function administracion(): Response
+    {
+        // Obtener todas las categorías únicas de los productos
+        $categorias = $this->entityManager->createQueryBuilder()
+            ->select('DISTINCT p.categoria')
+            ->from(Producto::class, 'p')
+            ->getQuery()
+            ->getResult();
 
+        $marcas = $this->entityManager->createQueryBuilder()
+            ->select('DISTINCT p.marca')
+            ->from(Producto::class, 'p')
+            ->getQuery()
+            ->getResult();
+
+        return $this->render('perfil/administracion.html.twig', [
+            'categorias' => $categorias,
+            'marcas' => $marcas
+        ]);
+    }
+    #[Route('/nuevoProducto', name: 'nuevoProducto')]
+    public function nuevoProducto(): Response
+    {
+        $producto = new Producto();
+        $producto->setNombre($_POST['nombre']);
+        $producto->setDescripcion($_POST['descripcion']);
+        $producto->setPrecio($_POST['precio']);
+        //$producto->setDescuento($_POST['descuento']);
+        $producto->setCategoria($_POST['categoria']);
+        $producto->setMarca($_POST['marca']);
+        $producto->setColor($_POST['color']);
+        $producto->setImagen("/uploads/productos/" . $_FILES['foto']['name']);
+        $producto->setStock($_POST['stock']);
+        //$producto->setEstado($_POST['estado']);
+
+        $this->entityManager->persist($producto);
+        $this->entityManager->flush();
+        if (isset($_FILES['foto']) && strlen($_FILES['foto']['name'])) {
+            $this->guardarArchivo($_FILES['foto']);
+        }
+        return $this->redirectToRoute('home');
+    }
+
+    private function guardarArchivo($file)
+    {
+        $filesystem = new Filesystem();
+        $folderPath = $this->getParameter('kernel.project_dir') . '/public/uploads/productos/';
+        if (!$filesystem->exists($folderPath)) {
+            $filesystem->mkdir($folderPath, 0777, true);
+        }
+        $filePath = $folderPath . $file['name'];
+        move_uploaded_file($file['tmp_name'], $filePath);
+    }
+
+    #[Route('/buscador', name: 'buscador')]
+    public function buscador(): Response
+    {
+        $parametro = strtolower($_POST['titulo']);
+
+        $productos = $this->buscarProducto($parametro);
+
+        return $this->render('home/producto.html.twig', [
+            'productos' => $productos
+        ]);
+    }
+
+    private function buscarProducto($parametro)
+    {
+        return $this->repo_producto->createQueryBuilder('p')
+            ->andWhere("LOWER(p.nombre) LIKE '%" . $parametro . "%'")
+            ->orWhere("LOWER(p.categoria) LIKE '%" . $parametro . "%'")
+            ->orWhere("LOWER(p.marca) LIKE '%" . $parametro . "%'")
+            ->getQuery()->getResult();
+    }
+    private function buscarUsuario($parametro)
+    {
+        return $this->repo_usuario->createQueryBuilder('u')
+            ->andWhere("LOWER(u.email) LIKE '%" . $parametro . "%'")
+            ->getQuery()->getResult();
+    }
+
+
+    #[Route('/buscarProducto', name: 'buscarProducto', methods: ['POST'])]
+    public function buscarProductos(Request $request): Response
+    {
+        $producto = strtolower($request->request->get('producto'));
+        $lista = $this->buscarProducto($producto);
+        return $this->json($lista);
+    }
+
+
+    #[Route('/buscarUsuario', name: 'buscarUsuario', methods: ['POST'])]
+    public function buscarUsuarios(Request $request): Response
+    {
+        $usuario = strtolower($request->request->get('usuario'));
+        $lista = $this->buscarUsuario($usuario);
+        return $this->json($lista);
+    }
+
+    #[Route('/eliminarUsuario', name: 'eliminarUsuario', methods: ['POST'])]
+    public function eliminarUsuario(Request $request): Response
+    {
+        $id = $request->request->get('id');
+        $user = $this->repo_usuario->find($id);
+        $this->entityManager->remove($user);
+        $this->entityManager->flush();
+        return $this->json($user);
+    }
+
+    #[Route('/eliminarProducto', name: 'eliminarProducto', methods: ['POST'])]
+    public function eliminarProducto(Request $request, SessionInterface $session): Response
+    {
+        $id = $request->request->get('id');
+        $product = $this->repo_producto->find($id);
+        $this->entityManager->remove($product);
+        $this->entityManager->flush();
+        return $this->json($product);
+    }
 }
